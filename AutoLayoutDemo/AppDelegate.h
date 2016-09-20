f6f40090d4802fdb73bb75fdc903c2fd0d410bf2
lsandro <42097314@qq.com>
2016-09-20 10:02:42 +0800

    Initial Commit

---
+++ AutoLayoutDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AutoLayoutDemo
+//
+//  Created by YI on 16/9/20.
+//  Copyright © 2016年 Sandro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AutoLayoutDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AutoLayoutDemo
+//
+//  Created by YI on 16/9/20.
+//  Copyright © 2016年 Sandro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
